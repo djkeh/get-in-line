921053c67bed597a7c8d520a601770955d6a15a6
Uno <djkehh@gmail.com>
2021-08-01 07:52:07 +0900

    Setup Spring Boot
    2.5.3 + java 16
    기본 구성은 web + actuator + lombok
    우선 이정도만

---
+++ src/test/java/com/uno/getinline/GetInLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uno.getinline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetInLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
