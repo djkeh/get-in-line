bd1fb578459c91068c732fbede43cf04011dcef8
Uno <djkehh@gmail.com>
2021-08-11 01:25:19 +0900

    Update #2 - admin view controllers and views
    어드민 뷰 메소드에 인자 입력 및 모델 전달 코드 추가
    관찰 가능하게끔 간단히 일부 뷰에 타임리프 템플릿 코드 추가

--- src/main/java/com/uno/getinline/controller/AdminController.java
+++ src/main/java/com/uno/getinline/controller/AdminController.java
@@ -1,17 +1,33 @@
 package com.uno.getinline.controller;
 
+import com.uno.getinline.constant.EventStatus;
+import com.uno.getinline.constant.PlaceType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
 
     @GetMapping("/places")
-    public String adminPlaces() {
-        return "admin/places";
+    public ModelAndView adminPlaces(
+            PlaceType placeType,
+            String placeName,
+            String address
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeType", placeType);
+        map.put("placeName", placeName);
+        map.put("address", address);
+
+        return new ModelAndView("admin/places", map);
     }
 
     @GetMapping("/places/{placeId}")
@@ -20,8 +36,21 @@ public class AdminController {
     }
 
     @GetMapping("/events")
-    public String adminEvents() {
-        return "admin/events";
+    public ModelAndView adminEvents(
+            Integer placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeName", "place-" + placeId);
+        map.put("eventName", eventName);
+        map.put("eventStatus", eventStatus);
+        map.put("eventStartDatetime", eventStartDatetime);
+        map.put("eventEndDatetime", eventEndDatetime);
+
+        return new ModelAndView("admin/events", map);
     }
 
     @GetMapping("/events/{eventId}")
