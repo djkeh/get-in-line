059ccd95be1ed752a168c330efff903497352dfb
Uno <djkehh@gmail.com>
2021-08-12 13:45:07 +0900

    Update #2 - BaseController and BaseErrorController
    베이스 컨트롤러에서 에러 처리 부분을 분리시킴
    에러 내용을 관찰할 수 있게끔 로직 고도화하고, 템플릿 코드 추가

---
+++ src/main/java/com/uno/getinline/controller/error/BaseErrorController.java
@@ -0,0 +1,45 @@
+package com.uno.getinline.controller.error;
+
+import com.uno.getinline.constant.ErrorCode;
+import com.uno.getinline.dto.APIErrorResponse;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Controller
+public class BaseErrorController implements ErrorController {
+
+    @RequestMapping(path = "/error", produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView errorHtml(HttpServletResponse response) {
+        HttpStatus status = HttpStatus.valueOf(response.getStatus());
+        ErrorCode errorCode = status.is4xxClientError() ? ErrorCode.BAD_REQUEST : ErrorCode.INTERNAL_ERROR;
+
+        return new ModelAndView(
+                "error",
+                Map.of(
+                        "statusCode", status.value(),
+                        "errorCode", errorCode,
+                        "message", errorCode.getMessage(status.getReasonPhrase())
+                ),
+                status
+        );
+    }
+
+    @RequestMapping("/error")
+    public ResponseEntity<APIErrorResponse> error(HttpServletResponse response) {
+        HttpStatus status = HttpStatus.valueOf(response.getStatus());
+        ErrorCode errorCode = status.is4xxClientError() ? ErrorCode.BAD_REQUEST : ErrorCode.INTERNAL_ERROR;
+
+        return ResponseEntity
+                .status(status)
+                .body(APIErrorResponse.of(false, errorCode));
+    }
+
+}
