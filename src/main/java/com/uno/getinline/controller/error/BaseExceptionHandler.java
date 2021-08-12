d20f07d10d828a07e9bee911ab01c28e3bb44bb4
Uno <djkehh@gmail.com>
2021-08-12 13:46:22 +0900

    Implement #2 - view controller advice
    뷰에 대응하는 `@ControllerAdvice` 구현
    프로젝트 정의 에러와 전체 에러를 잡는 구성

---
+++ src/main/java/com/uno/getinline/controller/error/BaseExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.uno.getinline.controller.error;
+
+import com.uno.getinline.constant.ErrorCode;
+import com.uno.getinline.exception.GeneralException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+    @ExceptionHandler
+    public ModelAndView general(GeneralException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        HttpStatus status = errorCode.isClientSideError() ?
+                HttpStatus.BAD_REQUEST :
+                HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return new ModelAndView(
+                "error",
+                Map.of(
+                        "statusCode", status.value(),
+                        "errorCode", errorCode,
+                        "message", errorCode.getMessage(e)
+                ),
+                status
+        );
+    }
+
+    @ExceptionHandler
+    public ModelAndView exception(Exception e) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return new ModelAndView(
+                "error",
+                Map.of(
+                        "statusCode", status.value(),
+                        "errorCode", errorCode,
+                        "message", errorCode.getMessage(e)
+                ),
+                status
+        );
+    }
+
+}
