ee34f2fbd3f38a7fb71737e8795224aef4164617
Uno <djkehh@gmail.com>
2021-08-24 04:08:10 +0900

    Test #2 - APIDataResponse
    빠져있던 테스트를 추가

---
+++ src/test/java/com/uno/getinline/dto/APIDataResponseTest.java
@@ -0,0 +1,44 @@
+package com.uno.getinline.dto;
+
+import com.uno.getinline.constant.ErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class APIDataResponseTest {
+
+    @DisplayName("문자열 데이터가 주어지면, 표준 성공 응답을 생성한다.")
+    @Test
+    void givenStringData_whenCreatingResponse_thenReturnsSuccessfulResponse() {
+        // Given
+        String data = "test data";
+
+        // When
+        APIDataResponse<String> response = APIDataResponse.of(data);
+
+        // Then
+        assertThat(response)
+                .hasFieldOrPropertyWithValue("success", true)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.OK.getCode())
+                .hasFieldOrPropertyWithValue("message", ErrorCode.OK.getMessage())
+                .hasFieldOrPropertyWithValue("data", data);
+    }
+
+    @DisplayName("데이터가 없을 때, 비어있는 표준 성공 응답을 생성한다.")
+    @Test
+    void givenNothing_whenCreatingResponse_thenReturnsEmptySuccessfulResponse() {
+        // Given
+
+        // When
+        APIDataResponse<String> response = APIDataResponse.empty();
+
+        // Then
+        assertThat(response)
+                .hasFieldOrPropertyWithValue("success", true)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.OK.getCode())
+                .hasFieldOrPropertyWithValue("message", ErrorCode.OK.getMessage())
+                .hasFieldOrPropertyWithValue("data", null);
+    }
+
+}
