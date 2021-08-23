f65d041e3bf2b5d1601b6e74088dca7b89985773
Uno <djkehh@gmail.com>
2021-08-24 04:31:46 +0900

    Refactor #2 - APIPlaceControllerTest
    TDD 실습하면서 테스트 작성한 내용을 반영
    
    * 장소 리스트 조회 API 테스트 메소드의 이름을 좀 더 친절하게
    * `contentTypeCompatibleWith()` -> `contentType()` 으로 검사 조건을 보다 특정하게 수정

--- src/test/java/com/uno/getinline/controller/api/APIPlaceControllerTest.java
+++ src/test/java/com/uno/getinline/controller/api/APIPlaceControllerTest.java
@@ -28,15 +28,15 @@ class APIPlaceControllerTest {
         this.mapper = mapper;
     }
 
-    @DisplayName("[API][GET] 장소 리스트 조회")
+    @DisplayName("[API][GET] 장소 리스트 조회 - 장소 리스트 데이터를 담은 표준 API 출력")
     @Test
-    void givenNothing_whenRequestingPlaces_thenReturnsListOfPlacesInStandardResponse() throws Exception {
+    void givenNothing_whenRequestingPlaces_thenReturnsPlacesInStandardResponse() throws Exception {
         // Given
 
         // When & Then
         mvc.perform(get("/api/places"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.data").isArray())
                 .andExpect(jsonPath("$.data[0].placeType").value(PlaceType.COMMON.name()))
                 .andExpect(jsonPath("$.data[0].placeName").value("랄라배드민턴장"))
@@ -69,7 +69,7 @@ class APIPlaceControllerTest {
                         .content(mapper.writeValueAsString(placeRequest))
         )
                 .andExpect(status().isCreated())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.errorCode").value(ErrorCode.OK.getCode()))
                 .andExpect(jsonPath("$.message").value(ErrorCode.OK.getMessage()));
@@ -84,7 +84,7 @@ class APIPlaceControllerTest {
         // When & Then
         mvc.perform(get("/api/places/" + placeId))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.data").isMap())
                 .andExpect(jsonPath("$.data.placeType").value(PlaceType.COMMON.name()))
                 .andExpect(jsonPath("$.data.placeName").value("랄라배드민턴장"))
@@ -106,7 +106,7 @@ class APIPlaceControllerTest {
         // When & Then
         mvc.perform(get("/api/places/" + placeId))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.data").isEmpty())
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.errorCode").value(ErrorCode.OK.getCode()))
@@ -134,7 +134,7 @@ class APIPlaceControllerTest {
                         .content(mapper.writeValueAsString(placeRequest))
         )
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.errorCode").value(ErrorCode.OK.getCode()))
                 .andExpect(jsonPath("$.message").value(ErrorCode.OK.getMessage()));
@@ -149,7 +149,7 @@ class APIPlaceControllerTest {
         // When & Then
         mvc.perform(delete("/api/places/" + placeId))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.errorCode").value(ErrorCode.OK.getCode()))
                 .andExpect(jsonPath("$.message").value(ErrorCode.OK.getMessage()));
