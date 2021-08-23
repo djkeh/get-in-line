b7e2f10c3f93b046255d285a2554bf96953ff436
Uno <djkehh@gmail.com>
2021-08-23 23:22:14 +0900

    Test #2 - AuthController, BaseController, BaseErrorController
    인증 관련 뷰, 기본 뷰, 에러 뷰의 테스트 작성

---
+++ src/test/java/com/uno/getinline/controller/AuthControllerTest.java
@@ -0,0 +1,49 @@
+package com.uno.getinline.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AuthController.class)
+class AuthControllerTest {
+
+    private final MockMvc mvc;
+
+    public AuthControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 로그인 페이지")
+    @Test
+    void givenNothing_whenRequestingLoginPage_thenReturnsLoginPage() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(content().string(containsString("This is login page.")))
+                .andExpect(view().name("auth/login"));
+    }
+
+    @DisplayName("[view][GET] 어드민 회원 가입 페이지")
+    @Test
+    void givenNothing_whenRequestingSignUpPage_thenReturnsSignUpPage() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/sign-up"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(content().string(containsString("This is sign-up page.")))
+                .andExpect(view().name("auth/sign-up"));
+    }
+
+}
---
+++ src/test/java/com/uno/getinline/controller/BaseControllerTest.java
@@ -0,0 +1,38 @@
+package com.uno.getinline.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(BaseController.class)
+class BaseControllerTest {
+
+    private final MockMvc mvc;
+
+    public BaseControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 기본 페이지 요청")
+    @Test
+    void givenNothing_whenRequestingRootPage_thenReturnsIndexPage() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(content().string(containsString("This is default page.")))
+                .andExpect(view().name("index"))
+                .andDo(print());
+    }
+
+}
