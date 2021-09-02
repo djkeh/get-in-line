41c44ff89d20102a8e0bbe180e104ec9d55e48ec
Uno <djkehh@gmail.com>
2021-09-03 08:47:54 +0900

    Implement #8 - event list view, event detail view
    타임리프를 적용하여 간단한 형태로 리스트 뷰, 상세 뷰 작성
    대응하는 컨트롤러에 임시 테스트 데이터

--- src/main/java/com/uno/getinline/controller/EventController.java
+++ src/main/java/com/uno/getinline/controller/EventController.java
@@ -1,22 +1,71 @@
 package com.uno.getinline.controller;
 
+import com.uno.getinline.constant.EventStatus;
+import com.uno.getinline.dto.EventResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/events")
 @Controller
 public class EventController {
 
     @GetMapping
-    public String events() {
-        return "event/index";
+    public ModelAndView events() {
+        Map<String, Object> map = new HashMap<>();
+
+        // TODO: 임시 데이터. 추후 삭제 예정
+        map.put("events", List.of(EventResponse.of(
+                1L,
+                1L,
+                "오후 운동",
+                EventStatus.OPENED,
+                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                0,
+                24,
+                "마스크 꼭 착용하세요"
+        ), EventResponse.of(
+                2L,
+                1L,
+                "오후 운동",
+                EventStatus.OPENED,
+                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                0,
+                24,
+                "마스크 꼭 착용하세요"
+                )
+        ));
+
+        return new ModelAndView("event/index", map);
     }
 
     @GetMapping("/{eventId}")
-    public String eventDetail(@PathVariable Long eventId) {
-        return "event/detail";
+    public ModelAndView eventDetail(@PathVariable Long eventId) {
+        Map<String, Object> map = new HashMap<>();
+
+        // TODO: 임시 데이터. 추후 삭제 예정
+        map.put("event", EventResponse.of(
+                eventId,
+                1L,
+                "오후 운동",
+                EventStatus.OPENED,
+                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                0,
+                24,
+                "마스크 꼭 착용하세요"
+        ));
+
+        return new ModelAndView("event/detail", map);
     }
 
 }
