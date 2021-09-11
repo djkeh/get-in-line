53cd4d7cba1b9d2b86d3b61d63fedb4f3f46823a
Uno <djkehh@gmail.com>
2021-09-12 05:00:30 +0900

    Refactor - GetInLineApplicationTests
    `@MockBean`과 같이 다른 컨텍스트 구성을 사용하는
    테스트들 때문에 전체 테스트가 느려지는 상황에서
    조금이라도 부담을 줄여서 더 빨리 테스트들이 실행될 수 있도록
    이 기본 테스트를 무시 처리.

--- src/test/java/com/uno/getinline/GetInLineApplicationTests.java
+++ src/test/java/com/uno/getinline/GetInLineApplicationTests.java
@@ -1,9 +1,11 @@
 package com.uno.getinline;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("다른 테스트의 속도 향상을 위해 비활성화")
 @DisplayName("스프링 부트 기본 테스트")
 @SpringBootTest
 class GetInLineApplicationTests {
