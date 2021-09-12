1cf473998a606a9778706be974b57e6c824772cc
Uno <djkehh@gmail.com>
2021-09-12 21:35:05 +0900

    Add #9 - repository interfaces
    각 도메인에 연결할 리포지토리 인터페이스 작성
    `JpaRepository`를 기본으로 하고
    `Event`는 수업에 사용할 목적이므로 필요한 기능을 좀 더 구현

--- src/main/java/com/uno/getinline/repository/EventRepository.java
+++ src/main/java/com/uno/getinline/repository/EventRepository.java
@@ -1,25 +1,26 @@
 package com.uno.getinline.repository;
 
-import com.uno.getinline.constant.EventStatus;
-import com.uno.getinline.dto.EventDTO;
+import com.querydsl.core.types.dsl.ComparableExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.uno.getinline.domain.Event;
+import com.uno.getinline.domain.QEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+public interface EventRepository extends
+        JpaRepository<Event, Long>,
+        QuerydslPredicateExecutor<Event>,
+        QuerydslBinderCustomizer<QEvent> {
 
-// TODO: 인스턴스 생성 편의를 위해 임시로 default 사용. repository layer 구현이 완성되면 삭제할 것
-public interface EventRepository {
-
-    default List<EventDTO> findEvents(
-            Long placeId,
-            String eventName,
-            EventStatus eventStatus,
-            LocalDateTime eventStartDatetime,
-            LocalDateTime eventEndDatetime
-    ) { return List.of(); }
-    default Optional<EventDTO> findEvent(Long eventId) { return Optional.empty(); }
-    default boolean insertEvent(EventDTO eventDTO) { return false; }
-    default boolean updateEvent(Long eventId, EventDTO dto) { return false; }
-    default boolean deleteEvent(Long eventId) { return false; }
+    @Override
+    default void customize(QuerydslBindings bindings, QEvent root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.placeId, root.eventName, root.eventStatus, root.eventStartDatetime, root.eventEndDatetime);
+        bindings.bind(root.eventName).first(StringExpression::likeIgnoreCase);
+        bindings.bind(root.eventStartDatetime).first(ComparableExpression::goe);
+        bindings.bind(root.eventEndDatetime).first(ComparableExpression::loe);
+    }
 
 }
