661d98840a04c07b1326b0f42b32bd728ca83618
Uno <djkehh@gmail.com>
2021-09-12 21:32:50 +0900

    Implement #9 - domain entities
    각 도메인마다 본격적으로 `@Entity` 지정하고
    JPA 기술 사용에 필요한 세부 요소를 적용
    
    * `getter()` 는 전체 적용
    * `setter()` 는 선택적으로 적용 (`id`, `created_at` 등은 임의로 세팅하지 못하도록)
    * 편리한 검사와 관찰을 위해 `@ToString` `@EqualsAndHashCode` 적용
    * 인덱스 표현
    * Auditable Entity 적용
    * ID의 설정
    * `created_at`, `modified_at` 의 처리 방법 설정
    * 기본 생성자 작성 - JPA 프레임워크 용이므로 열지 않고 `protected`
    * 데이터 생성자 작성 - `of()`를 사용하게끔 `protected`

--- src/main/java/com/uno/getinline/domain/Admin.java
+++ src/main/java/com/uno/getinline/domain/Admin.java
@@ -1,21 +1,74 @@
 package com.uno.getinline.domain;
 
-import lombok.Data;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@ToString
+@EqualsAndHashCode
+@Table(indexes = {
+        @Index(columnList = "phoneNumber"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "modifiedAt")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Admin {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    @Setter
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Setter
+    @Column(nullable = false, unique = true)
     private String nickname;
+
+    @Setter
+    @Column(nullable = false)
     private String password;
+
+    @Setter
+    @Column(nullable = false)
     private String phoneNumber;
+
+
+    @Setter
     private String memo;
 
+
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP")
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP")
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
+
+
+    protected Admin() {}
+
+    protected Admin(String email, String nickname, String password, String phoneNumber, String memo) {
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.memo = memo;
+    }
+
+    public static Admin of(String email, String nickname, String password, String phoneNumber, String memo) {
+        return new Admin(email, nickname, password, phoneNumber, memo);
+    }
 
 }
