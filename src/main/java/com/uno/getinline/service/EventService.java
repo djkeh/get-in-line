3cbf4d4fce1603436aeb9e6b212be7f47a95e391
Uno <djkehh@gmail.com>
2021-09-12 22:32:13 +0900

    Implement #9 - EventService to use JPA repository
    spring data jpa 를 도입하고 제대로 기능을 사용하도록
    테스트를 수정하고, 서비스 로직을 다시 구현
    
    직접 파라미터를 받고 처리했던 `findEvents()`와 같은
    메소드 디자인은 추후 다시 사용할 수도 있지만
    일단 지금 사용하지 않으므로 테스트에서 삭제

--- src/main/java/com/uno/getinline/service/EventService.java
+++ src/main/java/com/uno/getinline/service/EventService.java
@@ -1,5 +1,6 @@
 package com.uno.getinline.service;
 
+import com.querydsl.core.types.Predicate;
 import com.uno.getinline.constant.ErrorCode;
 import com.uno.getinline.constant.EventStatus;
 import com.uno.getinline.dto.EventDTO;
@@ -11,12 +12,23 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 @RequiredArgsConstructor
 @Service
 public class EventService {
 
     private final EventRepository eventRepository;
+
+    public List<EventDTO> getEvents(Predicate predicate) {
+        try {
+            return StreamSupport.stream(eventRepository.findAll(predicate).spliterator(), false)
+                    .map(EventDTO::of)
+                    .toList();
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
+    }
 
     public List<EventDTO> getEvents(
             Long placeId,
@@ -26,51 +38,57 @@ public class EventService {
             LocalDateTime eventEndDatetime
     ) {
         try {
-            return eventRepository.findEvents(
-                    placeId,
-                    eventName,
-                    eventStatus,
-                    eventStartDatetime,
-                    eventEndDatetime
-            );
-        }
-        catch (Exception e) {
+            return null;
+        } catch (Exception e) {
             throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
         }
     }
 
     public Optional<EventDTO> getEvent(Long eventId) {
         try {
-            return eventRepository.findEvent(eventId);
-        }
-        catch (Exception e) {
+            return eventRepository.findById(eventId).map(EventDTO::of);
+        } catch (Exception e) {
             throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
         }
     }
 
     public boolean createEvent(EventDTO eventDTO) {
         try {
-            return eventRepository.insertEvent(eventDTO);
-        }
-        catch (Exception e) {
+            if (eventDTO == null) {
+                return false;
+            }
+
+            eventRepository.save(eventDTO.toEntity());
+            return true;
+        } catch (Exception e) {
             throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
         }
     }
 
     public boolean modifyEvent(Long eventId, EventDTO dto) {
         try {
-            return eventRepository.updateEvent(eventId, dto);
-        }
-        catch (Exception e) {
+            if (eventId == null || dto == null) {
+                return false;
+            }
+
+            eventRepository.findById(eventId)
+                    .ifPresent(event -> eventRepository.save(dto.updateEntity(event)));
+
+            return true;
+        } catch (Exception e) {
             throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
         }
     }
 
     public boolean removeEvent(Long eventId) {
         try {
-            return eventRepository.deleteEvent(eventId);
-        }
-        catch (Exception e) {
+            if (eventId == null) {
+                return false;
+            }
+
+            eventRepository.deleteById(eventId);
+            return true;
+        } catch (Exception e) {
             throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
         }
     }
