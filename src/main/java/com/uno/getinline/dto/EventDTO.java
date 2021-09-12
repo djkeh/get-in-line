68547c746df1066450fbdcbbf1b2a50c96624d7d
Uno <djkehh@gmail.com>
2021-09-12 22:29:50 +0900

    Update #9 - EventDTO to handle Event entity
    `EventDTO` 가 `Event` 엔티티와 몇 가지 상황에서
    상호작용할 수 있도록 기능을 추가

--- src/main/java/com/uno/getinline/dto/EventDTO.java
+++ src/main/java/com/uno/getinline/dto/EventDTO.java
@@ -1,6 +1,7 @@
 package com.uno.getinline.dto;
 
 import com.uno.getinline.constant.EventStatus;
+import com.uno.getinline.domain.Event;
 
 import java.time.LocalDateTime;
 
@@ -17,6 +18,7 @@ public record EventDTO(
         LocalDateTime createdAt,
         LocalDateTime modifiedAt
 ) {
+
     public static EventDTO of(
             Long id,
             Long placeId,
@@ -43,5 +45,47 @@ public record EventDTO(
                 createdAt,
                 modifiedAt
         );
+    }
+
+    public static EventDTO of(Event event) {
+        return new EventDTO(
+                event.getId(),
+                event.getPlaceId(),
+                event.getEventName(),
+                event.getEventStatus(),
+                event.getEventStartDatetime(),
+                event.getEventEndDatetime(),
+                event.getCurrentNumberOfPeople(),
+                event.getCapacity(),
+                event.getMemo(),
+                event.getCreatedAt(),
+                event.getModifiedAt()
+        );
+    }
+
+    public Event toEntity() {
+        return Event.of(
+                placeId,
+                eventName,
+                eventStatus,
+                eventStartDatetime,
+                eventEndDatetime,
+                currentNumberOfPeople,
+                capacity,
+                memo
+        );
+    }
+
+    public Event updateEntity(Event event) {
+        if (placeId != null) { event.setPlaceId(placeId); }
+        if (eventName != null) { event.setEventName(eventName); }
+        if (eventStatus != null) { event.setEventStatus(eventStatus); }
+        if (eventStartDatetime != null) { event.setEventStartDatetime(eventStartDatetime); }
+        if (eventEndDatetime != null) { event.setEventEndDatetime(eventEndDatetime); }
+        if (currentNumberOfPeople != null) { event.setCurrentNumberOfPeople(currentNumberOfPeople); }
+        if (capacity != null) { event.setCapacity(capacity); }
+        if (memo != null) { event.setMemo(memo); }
+
+        return event;
     }
 }
