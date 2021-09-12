12545a4dae7efd04fcd9c75c7923cd4480c86875
Uno <djkehh@gmail.com>
2021-09-12 22:41:52 +0900

    Implement #9 - EventController to use EventService
    이제 jpa를 사용하므로,
    이벤트 뷰 컨트롤러가 제대로 서비스 로직을 통해
    데이터를 가져오게끔 수정

--- src/test/java/com/uno/getinline/controller/EventControllerTest.java
+++ src/test/java/com/uno/getinline/controller/EventControllerTest.java
@@ -1,12 +1,22 @@
 package com.uno.getinline.controller;
 
+import com.uno.getinline.domain.Event;
+import com.uno.getinline.dto.EventDTO;
+import com.uno.getinline.service.EventService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -16,6 +26,8 @@ class EventControllerTest {
 
     private final MockMvc mvc;
 
+    @MockBean private EventService eventService;
+
     public EventControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
@@ -24,6 +36,7 @@ class EventControllerTest {
     @Test
     void givenNothing_whenRequestingEventsPage_thenReturnsEventsPage() throws Exception {
         // Given
+        given(eventService.getEvents(any())).willReturn(List.of());
 
         // When & Then
         mvc.perform(get("/events"))
@@ -32,13 +45,17 @@ class EventControllerTest {
                 .andExpect(view().name("event/index"))
                 .andExpect(model().hasNoErrors())
                 .andExpect(model().attributeExists("events"));
+        then(eventService).should().getEvents(any());
     }
 
     @DisplayName("[view][GET] 이벤트 세부 정보 페이지")
     @Test
-    void givenNothing_whenRequestingEventDetailPage_thenReturnsEventDetailPage() throws Exception {
+    void givenEventId_whenRequestingEventDetailPage_thenReturnsEventDetailPage() throws Exception {
         // Given
         long eventId = 1L;
+        given(eventService.getEvent(eventId)).willReturn(Optional.of(
+                EventDTO.of(eventId, null, null, null, null, null, null, null, null, null, null)
+        ));
 
         // When & Then
         mvc.perform(get("/events/" + eventId))
@@ -47,6 +64,24 @@ class EventControllerTest {
                 .andExpect(view().name("event/detail"))
                 .andExpect(model().hasNoErrors())
                 .andExpect(model().attributeExists("event"));
+
+        then(eventService).should().getEvent(eventId);
+    }
+
+    @DisplayName("[view][GET] 이벤트 세부 정보 페이지 - 데이터 없음")
+    @Test
+    void givenNonexistentEventId_whenRequestingEventDetailPage_thenReturnsErrorPage() throws Exception {
+        // Given
+        long eventId = 0L;
+        given(eventService.getEvent(eventId)).willReturn(Optional.empty());
+
+        // When & Then
+        mvc.perform(get("/events/" + eventId))
+                .andExpect(status().isBadRequest()) // TODO: 나중에 404로 바꿔보자
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("error"));
+
+        then(eventService).should().getEvent(eventId);
     }
 
 }
