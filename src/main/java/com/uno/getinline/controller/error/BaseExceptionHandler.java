57b6cb5a86d90d579bb5fad87d9573d8041f40f9
Uno <djkehh@gmail.com>
2021-09-12 20:52:27 +0900

    Refactor - BaseExceptionHandler
    이미 `GeneralException`으로 들어오는 예외는
    예외를 다시 메시지로 넘겨줄 필요가 없음

--- src/main/java/com/uno/getinline/controller/error/BaseExceptionHandler.java
+++ src/main/java/com/uno/getinline/controller/error/BaseExceptionHandler.java
@@ -24,7 +24,7 @@ public class BaseExceptionHandler {
                 Map.of(
                         "statusCode", status.value(),
                         "errorCode", errorCode,
-                        "message", errorCode.getMessage(e)
+                        "message", errorCode.getMessage()
                 ),
                 status
         );
