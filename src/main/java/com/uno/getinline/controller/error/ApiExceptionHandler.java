f0125b0933b182de1f09e427d6720fbf47b4cbaf
Uno <djkehh@gmail.com>
2021-09-23 02:52:41 +0900

    Refactor - API, Base exception handlers
    `ErrorCode` 업데이트 후 예외 처리 핸들러 리팩토링
    공통점이 정리되면서 더욱 리팩토링할 수 있을 것 같다.

--- src/main/java/com/uno/getinline/controller/error/ApiExceptionHandler.java
+++ src/main/java/com/uno/getinline/controller/error/ApiExceptionHandler.java
@@ -20,40 +20,31 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
-        return handleExceptionInternal(e, ErrorCode.VALIDATION_ERROR, HttpHeaders.EMPTY, HttpStatus.BAD_REQUEST, request);
+        return handleExceptionInternal(e, ErrorCode.VALIDATION_ERROR, HttpHeaders.EMPTY, request);
     }
 
     @ExceptionHandler
     public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
-        ErrorCode errorCode = e.getErrorCode();
-        HttpStatus status = errorCode.isClientSideError() ?
-                HttpStatus.BAD_REQUEST :
-                HttpStatus.INTERNAL_SERVER_ERROR;
-
-        return handleExceptionInternal(e, errorCode, HttpHeaders.EMPTY, status, request);
+        return handleExceptionInternal(e, e.getErrorCode(), HttpHeaders.EMPTY, request);
     }
 
     @ExceptionHandler
     public ResponseEntity<Object> exception(Exception e, WebRequest request) {
-        return handleExceptionInternal(e, ErrorCode.INTERNAL_ERROR, HttpHeaders.EMPTY, HttpStatus.INTERNAL_SERVER_ERROR, request);
+        return handleExceptionInternal(e, ErrorCode.INTERNAL_ERROR, HttpHeaders.EMPTY, request);
     }
 
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ErrorCode errorCode = status.is4xxClientError() ?
-                ErrorCode.SPRING_BAD_REQUEST :
-                ErrorCode.SPRING_INTERNAL_ERROR;
-
-        return handleExceptionInternal(ex, errorCode, headers, status, request);
+        return handleExceptionInternal(ex, ErrorCode.valueOf(status), headers, request);
     }
 
 
-    private ResponseEntity<Object> handleExceptionInternal(Exception e, ErrorCode errorCode, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    private ResponseEntity<Object> handleExceptionInternal(Exception e, ErrorCode errorCode, HttpHeaders headers, WebRequest request) {
         return super.handleExceptionInternal(
                 e,
                 APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
                 headers,
-                status,
+                errorCode.getHttpStatus(),
                 request
         );
     }
--- src/main/java/com/uno/getinline/controller/error/BaseExceptionHandler.java
+++ src/main/java/com/uno/getinline/controller/error/BaseExceptionHandler.java
@@ -2,7 +2,6 @@ package com.uno.getinline.controller.error;
 
 import com.uno.getinline.constant.ErrorCode;
 import com.uno.getinline.exception.GeneralException;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,34 +14,30 @@ public class BaseExceptionHandler {
     @ExceptionHandler
     public ModelAndView general(GeneralException e) {
         ErrorCode errorCode = e.getErrorCode();
-        HttpStatus status = errorCode.isClientSideError() ?
-                HttpStatus.BAD_REQUEST :
-                HttpStatus.INTERNAL_SERVER_ERROR;
 
         return new ModelAndView(
                 "error",
                 Map.of(
-                        "statusCode", status.value(),
+                        "statusCode", errorCode.getHttpStatus().value(),
                         "errorCode", errorCode,
                         "message", errorCode.getMessage()
                 ),
-                status
+                errorCode.getHttpStatus()
         );
     }
 
     @ExceptionHandler
     public ModelAndView exception(Exception e) {
         ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
         return new ModelAndView(
                 "error",
                 Map.of(
-                        "statusCode", status.value(),
+                        "statusCode", errorCode.getHttpStatus().value(),
                         "errorCode", errorCode,
                         "message", errorCode.getMessage(e)
                 ),
-                status
+                errorCode.getHttpStatus()
         );
     }
 
