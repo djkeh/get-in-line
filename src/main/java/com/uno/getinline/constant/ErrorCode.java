3f2e5dc5b902debd4a261a083a22cffc1d095aab
Uno <djkehh@gmail.com>
2021-09-23 04:01:13 +0900

    Refactor - not found error status code: 400 -> 404
    아쉬운 TODO 였는데 이번 리팩토링을 통해 해결

--- src/main/java/com/uno/getinline/constant/ErrorCode.java
+++ src/main/java/com/uno/getinline/constant/ErrorCode.java
@@ -18,7 +18,7 @@ public enum ErrorCode {
     BAD_REQUEST(10000, HttpStatus.BAD_REQUEST, "Bad request"),
     SPRING_BAD_REQUEST(10001, HttpStatus.BAD_REQUEST, "Spring-detected bad request"),
     VALIDATION_ERROR(10002, HttpStatus.BAD_REQUEST, "Validation error"),
-    NOT_FOUND(10003, HttpStatus.BAD_REQUEST, "Requested resource is not found"),
+    NOT_FOUND(10003, HttpStatus.NOT_FOUND, "Requested resource is not found"),
 
     INTERNAL_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error"),
     SPRING_INTERNAL_ERROR(20001, HttpStatus.INTERNAL_SERVER_ERROR, "Spring-detected internal error"),
