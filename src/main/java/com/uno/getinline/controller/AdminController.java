5337651ae9cf45a38038bb39c7e06ce2f986a4ba
Uno <djkehh@gmail.com>
2021-10-10 11:12:51 +0900

    Implement #21 - admin view features
    어드민 장소, 이벤트 보기 기능을 구현
    그 밖에 간단한 이벤트 상세 페이지 오타 수정

--- src/main/java/com/uno/getinline/controller/AdminController.java
+++ src/main/java/com/uno/getinline/controller/AdminController.java
@@ -1,11 +1,23 @@
 package com.uno.getinline.controller;
 
+import com.querydsl.core.types.Predicate;
+import com.uno.getinline.constant.ErrorCode;
 import com.uno.getinline.constant.EventStatus;
 import com.uno.getinline.constant.PlaceType;
+import com.uno.getinline.domain.Event;
+import com.uno.getinline.domain.Place;
 import com.uno.getinline.dto.EventDto;
+import com.uno.getinline.dto.EventResponse;
 import com.uno.getinline.dto.PlaceDto;
+import com.uno.getinline.dto.PlaceResponse;
+import com.uno.getinline.exception.GeneralException;
+import com.uno.getinline.service.EventService;
+import com.uno.getinline.service.PlaceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,90 +25,77 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+@RequiredArgsConstructor
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
 
-    @GetMapping("/places")
-    public ModelAndView adminPlaces(
-            PlaceType placeType,
-            String placeName,
-            String address
-    ) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("placeType", placeType);
-        map.put("placeName", placeName);
-        map.put("address", address);
+    private final EventService eventService;
+    private final PlaceService placeService;
 
-        return new ModelAndView("admin/places", map);
+    @GetMapping("/places")
+    public ModelAndView adminPlaces(@QuerydslPredicate(root = Place.class) Predicate predicate) {
+        List<PlaceResponse> places = placeService.getPlaces(predicate)
+                .stream()
+                .map(PlaceResponse::from)
+                .toList();
+
+        return new ModelAndView(
+                "admin/places",
+                Map.of(
+                        "places", places,
+                        "placeType", PlaceType.values()
+                )
+        );
     }
 
     @GetMapping("/places/{placeId}")
     public ModelAndView adminPlaceDetail(@PathVariable Long placeId) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("place", PlaceDto.of(
-                placeId,
-                PlaceType.COMMON,
-                "랄라배드민턴장",
-                "서울시 강남구 강남대로 1234",
-                "010-1234-5678",
-                30,
-                "신장개업",
-                LocalDateTime.now(),
-                LocalDateTime.now()
-        ));
+        PlaceResponse place = placeService.getPlace(placeId)
+                .map(PlaceResponse::from)
+                .orElseThrow(() -> new GeneralException(ErrorCode.NOT_FOUND));
 
-        return new ModelAndView("admin/place-detail", map);
+        return new ModelAndView(
+                "admin/place-detail",
+                Map.of(
+                        "place", place,
+                        "placeType", PlaceType.values()
+                )
+        );
     }
 
     @GetMapping("/events")
-    public ModelAndView adminEvents(
-            Long placeId,
-            String eventName,
-            EventStatus eventStatus,
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime eventStartDatetime,
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime eventEndDatetime
-    ) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("placeName", "place-" + placeId);
-        map.put("eventName", eventName);
-        map.put("eventStatus", eventStatus);
-        map.put("eventStartDatetime", eventStartDatetime);
-        map.put("eventEndDatetime", eventEndDatetime);
+    public ModelAndView adminEvents(@QuerydslPredicate(root = Event.class) Predicate predicate) {
+        List<EventResponse> events = eventService.getEvents(predicate)
+                .stream()
+                .map(EventResponse::from)
+                .toList();
 
-        return new ModelAndView("admin/events", map);
+        return new ModelAndView(
+                "admin/events",
+                Map.of(
+                        "events", events,
+                        "eventStatus", EventStatus.values()
+                )
+        );
     }
 
     @GetMapping("/events/{eventId}")
     public ModelAndView adminEventDetail(@PathVariable Long eventId) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("event", EventDto.of(
-                eventId,
-                PlaceDto.of(
-                        1L,
-                        PlaceType.SPORTS,
-                        "배드민턴장",
-                        "서울시 그리구 그래동",
-                        "010-2222-3333",
-                        33,
-                        null,
-                        LocalDateTime.now(),
-                        LocalDateTime.now()
-                ),
-                "오후 운동",
-                EventStatus.OPENED,
-                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
-                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
-                0,
-                24,
-                "마스크 꼭 착용하세요",
-                LocalDateTime.now(),
-                LocalDateTime.now()
-        ));
+        EventResponse event = eventService.getEvent(eventId)
+                .map(EventResponse::from)
+                .orElseThrow(() -> new GeneralException(ErrorCode.NOT_FOUND));
 
-        return new ModelAndView("admin/event-detail", map);
+        return new ModelAndView(
+                "admin/event-detail",
+                Map.of(
+                        "event", event,
+                        "eventStatus", EventStatus.values()
+                )
+        );
     }
 
 }
--- src/test/java/com/uno/getinline/controller/AdminControllerTest.java
+++ src/test/java/com/uno/getinline/controller/AdminControllerTest.java
@@ -2,16 +2,28 @@ package com.uno.getinline.controller;
 
 import com.uno.getinline.constant.EventStatus;
 import com.uno.getinline.constant.PlaceType;
+import com.uno.getinline.domain.Place;
+import com.uno.getinline.dto.EventDto;
+import com.uno.getinline.dto.PlaceDto;
+import com.uno.getinline.service.EventService;
+import com.uno.getinline.service.PlaceService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 어드민")
@@ -19,6 +31,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AdminControllerTest {
 
     private final MockMvc mvc;
+
+    @MockBean private EventService eventService;
+    @MockBean private PlaceService placeService;
 
     public AdminControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -28,6 +43,7 @@ class AdminControllerTest {
     @Test
     void givenQueryParams_whenRequestingAdminPlacesPage_thenReturnsAdminPlacesPage() throws Exception {
         // Given
+        given(placeService.getPlaces(any())).willReturn(List.of());
 
         // When & Then
         mvc.perform(
@@ -38,7 +54,12 @@ class AdminControllerTest {
         )
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(view().name("admin/places"));
+                .andExpect(view().name("admin/places"))
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attributeExists("places"))
+                .andExpect(model().attributeExists("placeType"));
+
+        then(placeService).should().getPlaces(any());
     }
 
     @DisplayName("[view][GET] 어드민 페이지 - 장소 세부 정보 뷰")
@@ -46,18 +67,59 @@ class AdminControllerTest {
     void givenPlaceId_whenRequestingAdminPlaceDetailPage_thenReturnsAdminPlaceDetailPage() throws Exception {
         // Given
         long placeId = 1L;
+        given(placeService.getPlace(placeId)).willReturn(Optional.of(
+                PlaceDto.of(placeId, null, null, null, null, null, null, null, null)
+        ));
 
         // When & Then
         mvc.perform(get("/admin/places/" + placeId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(view().name("admin/place-detail"));
+                .andExpect(view().name("admin/place-detail"))
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attributeExists("place"))
+                .andExpect(model().attributeExists("placeType"));
+    }
+
+    @DisplayName("[view][GET] 어드민 페이지 - 장소 세부 정보 뷰, 데이터 없음")
+    @Test
+    void givenNonexistentPlaceId_whenRequestingAdminPlaceDetailPage_thenReturnsErrorPage() throws Exception {
+        // Given
+        long placeId = 1L;
+        given(placeService.getPlace(placeId)).willReturn(Optional.empty());
+
+        // When & Then
+        mvc.perform(get("/admin/places/" + placeId))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("error"));
+        then(placeService).should().getPlace(placeId);
+    }
+
+    @DisplayName("[view][POST] 어드민 페이지 - 장소 세부 정보 뷰, 장소 저장")
+    @Test
+    void givenNewPlace_whenSavingAdminPlaceDetail_thenSavesPlaceAndReturnsToListPage() throws Exception {
+        // Given
+        Place
+        given(placeService.createPlace(placeId)).willReturn(Optional.empty());
+
+        // When & Then
+        mvc.perform(
+                post("/admin/places/" + placeId)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content()
+        )
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("error"));
+        then(placeService).should().getPlace(placeId);
     }
 
     @DisplayName("[view][GET] 어드민 페이지 - 이벤트 리스트 뷰")
     @Test
     void givenQueryParams_whenRequestingAdminEventsPage_thenReturnsAdminEventsPage() throws Exception {
         // Given
+        given(eventService.getEvents(any())).willReturn(List.of());
 
         // When & Then
         mvc.perform(
@@ -72,7 +134,11 @@ class AdminControllerTest {
         )
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(view().name("admin/events"));
+                .andExpect(view().name("admin/events"))
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attributeExists("events"))
+                .andExpect(model().attributeExists("eventStatus"));
+        then(eventService).should().getEvents(any());
     }
 
     @DisplayName("[view][GET] 어드민 페이지 - 이벤트 세부 정보 뷰")
@@ -80,12 +146,34 @@ class AdminControllerTest {
     void givenEventId_whenRequestingAdminEventDetailPage_thenReturnsAdminEventDetailPage() throws Exception {
         // Given
         long eventId = 1L;
+        given(eventService.getEvent(eventId)).willReturn(Optional.of(
+                EventDto.of(eventId, null, null, null, null, null, null, null, null, null, null)
+        ));
 
         // When & Then
         mvc.perform(get("/admin/events/" + eventId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(view().name("admin/event-detail"));
+                .andExpect(view().name("admin/event-detail"))
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attributeExists("event"))
+                .andExpect(model().attributeExists("eventStatus"));
+        then(eventService).should().getEvent(eventId);
+    }
+
+    @DisplayName("[view][GET] 어드민 페이지 - 이벤트 세부 정보 뷰, 데이터 없음")
+    @Test
+    void givenNonexistentEventId_whenRequestingAdminEventDetailPage_thenReturnsErrorPage() throws Exception {
+        // Given
+        long eventId = 1L;
+        given(eventService.getEvent(eventId)).willReturn(Optional.empty());
+
+        // When & Then
+        mvc.perform(get("/admin/events/" + eventId))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("error"));
+        then(eventService).should().getEvent(eventId);
     }
 
 }
