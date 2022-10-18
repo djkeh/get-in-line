ee7900e37f67c6a80c59bb132bb555fd81ac631e
Ezra Lim <skywno@gmail.com>
2022-10-18 17:46:25 +0300

    Update SecurityConfig
    Upgraded Deprecated WebSecurityConfigurerAdapter.

--- src/main/java/com/uno/getinline/config/SecurityConfig.java
+++ src/main/java/com/uno/getinline/config/SecurityConfig.java
@@ -3,45 +3,53 @@ package com.uno.getinline.config;
 import com.uno.getinline.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Autowired
-    public void configureGlobal(
-            AuthenticationManagerBuilder auth,
-            PasswordEncoder passwordEncoder,
-            AdminService adminService
+    @Bean
+    public AuthenticationManager authManager(
+            HttpSecurity http,
+            @Autowired PasswordEncoder passwordEncoder,
+            @Autowired AdminService adminService
     ) throws Exception {
-        auth.userDetailsService(adminService).passwordEncoder(passwordEncoder);
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(adminService)
+                .passwordEncoder(passwordEncoder)
+                .and()
+                .build();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/events/**", "/places/**").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                .formLogin()
+                .antMatchers("/", "/events/**", "/places/**")
                     .permitAll()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/admin/places")
+                .anyRequest()
+                    .authenticated()
+                .and()
+                .formLogin() // enable form based log in
+                    .permitAll()
                 .and()
                 .logout()
                     .permitAll()
-                    .logoutUrl("/logout")
                     .logoutSuccessUrl("/");
+        return http.build();
     }
-
 }
